9f2830c2e01153f633086a2436c9810195a42105
AbolfazlMohammady <abolfazlking22@gmail.com>
2024-09-30 04:42:10 -0700

    CustomUser

---
+++ core/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model= CustomUser
+    add_form= CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display= UserAdmin.list_display
